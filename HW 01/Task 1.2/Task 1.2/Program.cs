624a675d8bc227da22ed076b276a0488ba9541bc
Derek Ayala <derek.ayala12345@hotmail.com>
2023-11-06 10:19:13 -0600

    Task 1.2

---
+++ HW 01/Task 1.2/Task 1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first 9 digits of the ISBN:");
+        string input = Console.ReadLine();
+
+        int checkDigit = CalculateCheckDigit(input);
+        string isbn = input + (checkDigit == 10 ? "X" : checkDigit.ToString());
+        Console.WriteLine("Resulting ISBN: " + isbn);
+    }
+
+    static int CalculateCheckDigit(string input)
+    {
+        int sum = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            sum += (10 - i) * int.Parse(input[i].ToString());
+        }
+
+        int checkDigit = (11 - (sum % 11)) % 11;
+        return checkDigit;
+    }
+}
